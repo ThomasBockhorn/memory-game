76ccb92709eca81f1e20fda0770d20813377c00d
Thomas Bockhorn <thomasbockhorn1863@gmail.com>
2018-05-21 22:04:02 -0500

    Fixed javascript for card flipping

--- js/memory.js
+++ js/memory.js
@@ -1,6 +1,8 @@
 //will flip when card is picked
 const deck = document.querySelector(".deck");
 deck.addEventListener("click", function(e){
-    const side = e.target.parentElement.parentElement;
-    side.classList.toggle("flip");
-})
+    if(e.target.tagName.toLowerCase() === "img"){
+        const side = e.target.parentElement.parentElement;
+        side.classList.toggle("flip");
+    }
+});
