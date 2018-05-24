61713e482bed897f81351a31ada0716b7a0938c7
Thomas Bockhorn <thomasbockhorn1863@gmail.com>
2018-05-24 08:07:43 -0500

    reformulated the original pick() to flip()

--- js/memory.js
+++ js/memory.js
@@ -1,18 +1,17 @@
+//Global variable
+const arr = [];
+
 //When the page is loaded the cards get shuffled
 onload = shuffle();
 
-//Card functionality
-//this function pick will flip when card is picked
-function pick(){
-    const deck = document.querySelector(".deck");
-    deck.addEventListener("click", function(e){
-        if(e.target.tagName.toLowerCase() === "img"){
-            const side = e.target.parentElement.parentElement;
-            side.classList.toggle("flip");
-        }
-    });
+//flip() flips the cards
+function flip(e){
+     if(e.target.tagName.toLowerCase() === "img"){
+        const side = e.target.parentElement.parentElement;
+        side.classList.toggle("flip");
+    }
+    return e.target;
 }
-pick();
 
 //This function will shuffle the cards
 function shuffle(){
@@ -32,3 +31,14 @@ newChoice.addEventListener("click", function(){
     document.location.reload(true);
 });
 
+//When the user clicks a card
+const deck = document.querySelector(".deck");
+deck.addEventListener("click",function(e){
+    //put value in array then compare
+    const cardOne = flip(e);
+});
+//Hold on to the choice the user picked
+//const cardChoice = e.target;
+//arr.push(cardChoice.parentElement.nextElementSibling.innerHTML);
+//const deck = document.querySelector(".deck");
+//   deck.addEventListener("click", function(e){
