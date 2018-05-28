6659dd331b64ed2ba9f174ca49072b19e62aad66
Thomas Bockhorn <thomasbockhorn1863@gmail.com>
2018-05-28 15:04:21 -0500

    Set up message modal

--- js/memory.js
+++ js/memory.js
@@ -1,10 +1,18 @@
 //Global variable
+//For the memory game
 let cardMemory = [];
 let eventMemory = [];
 let counter = 0;
 const deck = document.querySelector(".deck");
 let winningCounter = 0;
 const runningTotal = document.querySelector(".display-Result");
+
+//for the modal
+const yes = document.querySelector("#yes");
+const no = document.querySelector("#no");
+const modalMessage = document.querySelector(".modal");
+const spanModal = document.querySelector(".close");
+const content = document.querySelector(".content");
 
 //When the page is loaded the cards get shuffled
 onload = shuffle();
@@ -45,10 +53,43 @@ function pickTwo(e){
         }  
     }
 }
+
+//This will clear the memory
 function clearMemory(){
     cardMemory.length = 0;
     eventMemory.length = 0;
     counter = 0;
+}
+
+//This will display the modal message if reached 8 pair
+function winningModal(){
+    //This will display the winning message when you reach 8 pairs
+    //displays when you reach 8 pairs
+    modalMessage.style.display = "block";
+
+    //When user clicks on the close button
+    span.addEventListener("click", function(){
+        modalMessage.style.display = "none";
+    });
+
+    //when user clicks outside of the box
+    window.onclick = function(e){
+        if(e.target == messageModal){
+            modalMessage.style.display = "none";
+        }
+    }
+
+    //When user clicks on yes
+    yes.addEventListener("click", function(){
+        document.location.reload(true); 
+        console.log("works!");
+    });
+
+    //When user clicks no
+    no.addEventListener("click", function(){
+        content.innerHTML = "Thanks for playing!  GOOD BYE!!";
+        console.log("works");
+    }); 
 }
 
 //function that compares the cards
@@ -58,6 +99,11 @@ function compare(){
             eventMemory[i].parentElement.nextElementSibling.classList.toggle("locking-card");
         }
         winningCounter++;
+        if(winningCounter === 8){
+            //This will display the winning message when you reach 8 pairs
+            //displays when you reach 8 pairs
+            modalMessage.style.display = "block";
+        }
         runningTotal.innerHTML = "You have paired: " + winningCounter;
         clearMemory();
     }else if(cardMemory[0] != cardMemory[1] && cardMemory.length === 2) {
@@ -66,16 +112,27 @@ function compare(){
         }
         clearMemory();
     }
-
-    //This will display the winning message when you reach 8 pairs
-    if(winningCounter === 8){
-        runningTotal.innerHTML = "YOU WON!!!"; 
-    }
 }
     
 //When a user picks two cards
 deck.addEventListener("click", function(e){
     flip(e);
     pickTwo(e);
-    setTimeout(compare,3000);  
+    setTimeout(compare,3000);
 });
+
+//Modal setup
+//When user clicks on the close button
+spanModal.addEventListener("click", function(){
+    modalMessage.style.display = "none";
+});
+
+//When user clicks on yes
+yes.addEventListener("click", function(){
+    document.location.reload(true); 
+});
+
+//When user clicks no
+no.addEventListener("click", function(){
+    content.innerHTML = "Thanks for playing!  GOOD BYE!!";
+}); 
